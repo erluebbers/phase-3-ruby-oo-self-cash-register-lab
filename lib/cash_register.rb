c7d312275dcd5f08fd497ed9dcdc6fedaa392ffe
Erik Luebbers <erluebbers@gmail.com>
2022-02-13 13:22:21 -0800

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0, discount)
+    @total = total
+    @discount = discount
+  end
 
+  def add_item(title, price)
+    
+  end
+
+
+end
